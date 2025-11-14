6ce0da86c07034a51b6c368aaa63f9bf8267c7a8
sriharshitaneyyala-14 <sriharshitaneyyala@gmail.com>
2025-11-14 11:17:05 +0530

    Add files via upload

---
+++ prime num range.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,count=0,n;
+    printf("enter n value: ");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        count=0;
+        for(j=1;j<=i;j++)
+        {
+            if (i%j == 0)
+            {
+                count++;
+            }
+        }
+        if (count==2)
+        {
+            printf("\n%d is a prime number",i);
+        }
+        else
+        {
+            printf("\n%d is not a prime number",i);
+        }
+    }
+    return 0;
+}
