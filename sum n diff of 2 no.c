42c08c292291bdfc908529d119d29c3ee4c14883
sriharshitaneyyala-14 <sriharshitaneyyala@gmail.com>
2025-11-14 10:07:59 +0530

    Add files via upload

---
+++ sum n diff of 2 no.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{int a,b,sum,diff;
+printf("enter a and b values");
+scanf("%d %d",&a,&b);
+sum=a+b;
+diff=a-b;
+printf("\nsum of digits=%d",sum);
+printf("\ndiff of digits=%d",diff);
+}
