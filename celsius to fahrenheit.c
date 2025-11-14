2bb5dc64d9f150d94ab3891b9e9fc1221d577642
sriharshitaneyyala-14 <sriharshitaneyyala@gmail.com>
2025-11-14 09:44:59 +0530

    Add files via upload

---
+++ celsius to fahrenheit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{float fahrenheit,celsius;
+printf("\ntemperation in celsius");
+scanf("%f",&celsius);
+fahrenheit=(celsius*1.8)+32;
+printf("\ntemperature in fahrenheit %f",fahrenheit);
+return 0;
+}
