578c7823febc55123a73735a5197108b3db2cebf
sriharshitaneyyala-14 <sriharshitaneyyala@gmail.com>
2025-11-14 09:57:31 +0530

    Add files via upload

---
+++ squareroot.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{double num, root;
+printf("enter the integer number");
+scanf("%lf",&num);
+root = sqrt(num);
+print("\n the square root of %.2lf is %.2lf", num root);
+return 0;
+}
